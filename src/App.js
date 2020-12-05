d22236216401788ab723332b65cec60c733dbb4f
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-12-04 19:44:19 -0500

    used AppBar from material Ui to make a basic header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Transactions from './components/Transactions'
-
+import Header from './components/Header'
 
 
 function App() {
   return (
     <div className="App">
-    <h1>hello world</h1>  
+   <Header/>
+   <br></br>
+   <br></br>
     <Transactions/>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {AppBar} from '@material-ui/core'
+const Header =()=>{
+    return(
+<AppBar style={{backgroundColor: 'blueviolet'}}>
+<h3>Finance app</h3>
+</AppBar>
+    )
+}
+
+export default Header
