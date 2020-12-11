c67e452a3a528dc4a21a4bd6308b5c5876ddf15d
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-12-11 18:08:11 -0500

    added client side routing via react-router dom

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import Transactions from './components/Transactions'
-import Header from './components/Header'
-import AccountManagement from './components/accountManagement'
+import AppRouter from './Routers/appRouter'
+
 
 function App() {
   return (
     <div className="App">
-   <Header/>
-   <br></br>
-   <br></br>
-    <AccountManagement/>
    
-
+   
+<AppRouter/>
     </div>
   );
 }
