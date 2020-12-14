b810f2d3e2ae3e6428c8a34279df68f45ccb9317
Darique Warren <60529780+dariquewarren@users.noreply.github.com>
2020-12-13 22:11:24 -0500

    changed api call to point to production api

--- src/Functions/objectServer.js
+++ src/Functions/objectServer.js
@@ -1,6 +1,6 @@
 export let corsUrl = 'https://cors-anywhere.herokuapp.com/'
-export let transactionsUrl = 'https://testwarrenfinanceapi.azurewebsites.net/api/Transactions'
-export let bankAccountsUrl = 'https://testwarrenfinanceapi.azurewebsites.net/api/BankAccounts'
+export let transactionsUrl = 'https://hwfinanceapp20201201223059.azurewebsites.net/api/Transactions'
+export let bankAccountsUrl = 'https://hwfinanceapp20201201223059.azurewebsites.net/api/BankAccounts'
 
 export let objectServer =(urlBase, params='')=>{
       
